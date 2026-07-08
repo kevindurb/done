1ee8474d235bd4ba07d2c48b043f4862531cb361
Kevin Durbin <kevindurb@fastmail.com>
2026-07-08 08:07:39 -0600

    add dotenv

--- main.go
+++ main.go
@@ -1,16 +1,17 @@
 package main
 
 import (
+	"log"
 	"net/http"
 
+	_ "github.com/joho/godotenv/autoload"
 	"github.com/kevindurb/done/app"
 	"github.com/kevindurb/done/config"
 )
 
 func main() {
 	c := config.FromEnv()
-
 	a := app.New(&c)
-
+	log.Printf("Listening: %s", c.ListenAddr)
 	http.ListenAndServe(c.ListenAddr, a.Router())
 }
